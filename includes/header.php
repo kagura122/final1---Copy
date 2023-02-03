bac470d2307230836564b69d84ef360ff8b142ae
AndreMickael <mollycupcake63@gmail.com>
2023-02-04 07:24:55 +0800

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/dashboard.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/global.css" />
+    <title><?php echo $page_title; ?></title>
+</head>
+<body>
---
+++ includes/sidebar.php
@@ -0,0 +1,35 @@
+<div class = "container-center-horizontal">
+<div class = "dashboard screen">
+<div class ="overlap-group-container">
+<div class ="overlap-group"> <!-- should be  overlap group 2-->
+<div class = "overlap-group1"><!--should be overlap group 2-->
+<div class ="dashboard-1 poppins-normal-black-24px">Dashboard</div>
+<img class ="logo" src = "image/logo.jpg" alt ="logo"/>
+
+<a href = "" >
+    <div class = "dashboard poppins-normal-black-24px">Dashboard</div>
+</a>
+<a href ="../fees/fees.php" class = "align-self-flex-start" class ="<?php echo $fees;?>">
+<span class = "fees">Fees</span> 
+</a>
+<a href = "../payment-records/payment-records.php" class ="align-self-flex-end">
+<div class = "payment-records poppins-normal-black-24px">Payment Records</div>
+</a>
+<a href = "../colleges/colleges.php" >
+    <div class = "colleges poppins-normal-black-24px">Colleges</div>
+</a>
+<a href = "../funds/funds.php" >
+    <div class = "funds poppins-normal-black-24px">Funds</div>
+</a>
+<a href = "../financial-report/financial-report.php" >
+    <div class = "financial-report poppins-normal-black-24px">Financial Report</div>
+</a>
+<a href = "../audit-log/audit-log.php" >
+    <div class = "audit-log poppins-normal-black-24px">Audit log</div>
+</a>
+<a href = "../admin-settings/admin-settings.php" >
+    <div class = "admin-settings poppins-normal-black-24px">Admin Settings</div>
+</a>
+<a href = "" >
+    <div class = "logout poppins-normal-black-24px">Logout</div></a>
+</div>
